c8c3ab34d90ee8c81f51619363b1fdf508c14345
hanhut0710 <huynhanhnhut94@gmail.com>
2024-10-29 14:44:20 +0700

    Thêm tính năng login của trang admin

--- admin/admin.js
+++ admin/admin.js
@@ -8,3 +8,20 @@ $(document).ready(function(){
             $(this).prev().attr('type', 'password');
     });
 });
+document.querySelector('.form-login').addEventListener('submit',function(event){
+    event.preventDefault();
+    const username = document.getElementById('username');
+    const password = document.getElementById('password');
+    if (username && password) {
+        const usernameValue = username.value;
+        const passwordValue = password.value;
+    if((usernameValue == 'hanhut' && passwordValue == 'hanhut123')
+    ||  (usernameValue == 'svngnhuan' && passwordValue == 'svngnhuan123')
+    ||  (usernameValue == 'svnguyen' && passwordValue == 'svnguyen123') 
+    ||  (usernameValue == 'svminleo' && passwordValue == 'svminleo123'))
+        window.location.href='../admin.html';
+    else
+        alert('Tên tài khoản hoặc mật khẩu không đúng');
+    }else
+        console.error('loiii');
+});
